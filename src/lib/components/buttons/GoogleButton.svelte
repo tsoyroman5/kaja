af3185791a57f20b3c4a1ceb44f2ec843f5c97c9
tsoyroman <tsoyroman5@gmail.com>
2025-08-11 10:41:11 +0900

    GoogleButton component updated

--- src/lib/components/buttons/GoogleButton.svelte
+++ src/lib/components/buttons/GoogleButton.svelte
@@ -33,9 +33,11 @@
 
 <style>
 	.gsi-material-button {
+		user-select: none;
 		-moz-user-select: none;
 		-webkit-user-select: none;
 		-ms-user-select: none;
+		appearance: none;
 		-webkit-appearance: none;
 		background-color: WHITE;
 		background-image: none;
